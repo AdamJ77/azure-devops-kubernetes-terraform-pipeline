a67abbdd9b15ae1b149e04f4e8a26f3d0ffbe8cb
Adam <01169179@pw.edu.pl>
2023-07-31 14:11:42 +0200

    Changed node count & added comments

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -1,34 +1,39 @@
+# wszystkie resource stworzone dla azure, muszą należeć do jakiejś resource group -> zatem musimy ją najpierw stworzyć
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
 }
 
+# azurerm -> azure resource manager
 provider "azurerm" {
   //version = "~>2.0.0"
   features {}
 }
 
+# pozwala na zarządzania klastrem kubernetes cluster w azure (czyli AKS - Azure Kubernetes Service)
+# pamiętamy by związać go z konkretną resource group (którą stworzyliśmy wcześniej)
 resource "azurerm_kubernetes_cluster" "terraform-k8s" {
-  name                = "${var.cluster_name}_${var.environment}"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  dns_prefix          = var.dns_prefix
+  name                = "${var.cluster_name}_${var.environment}"       # nazwa klastra
+  location            = azurerm_resource_group.resource_group.location # dajemy tą samą lokalizację do resource group do którego należy
+  resource_group_name = azurerm_resource_group.resource_group.name     # przypisujemy do resource grupy
+  dns_prefix          = var.dns_prefix                                 # obowiązkowe
 
-  linux_profile {
+  linux_profile { # tworzymy admina
     admin_username = "ubuntu"
 
     ssh_key {
-      key_data = file(var.ssh_public_key)
+      key_data = file(var.ssh_public_key) # aby móc się łączyć poprzez ssh do tego klastra?
     }
   }
 
-  default_node_pool {
-    name            = "agentpool"
-    node_count      = var.node_count
-    vm_size         = "standard_b2ms"
+  default_node_pool {            # node w kubernetes (ile zasobów obliczeniowych w klastrze)
+    name       = "agentpool"     # nazwa noda
+    node_count = var.node_count  # ustawiamy na 3 instancje
+    vm_size    = "standard_b2ms" # konkretna konfiguracja maszyny wirtualnej
     # vm_size         = "standard_d2as_v5"      CHANGE IF AN ERROR ARISES 
   }
 
+  # potrzebne do komunikacji z Azure (czyli też z klastrem)
   service_principal {
     client_id     = var.client_id
     client_secret = var.client_secret
@@ -39,8 +44,14 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   }
 }
 
+# Azurerm backend
+# używamy specjalnego backendu w azure by móc przechowywać tam pliki .tfstate,
+# żeby inni mieli do nich dostęp, żeby nie było wyścigu, itp.
 terraform {
   backend "azurerm" {
+    # tworzymy storage account, za pomocą którego tworzymy storage account container
+    # który będzie przechowywał pliki .tfstate
+
     # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
     # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
--- configuration/iaac/azure/kubernetes/variables.tf
+++ configuration/iaac/azure/kubernetes/variables.tf
@@ -1,29 +1,31 @@
-variable client_id {}
-variable client_secret {}
-variable ssh_public_key {}
+# oba należą do service account
+variable "client_id" {} # potrzebujemy do tego nasz subscription ID (na stronie azure mamy info)
+variable "client_secret" {}
 
-variable environment {
-    default = "dev"
+variable "ssh_public_key" {}
+
+variable "environment" {
+  default = "dev"
 }
 
-variable location {
-    default = "westeurope"
+variable "location" {
+  default = "westeurope"
 }
 
-variable node_count {
-  default = 3
+variable "node_count" {
+  default = 2
 }
 
 
 
-variable dns_prefix {
+variable "dns_prefix" {
   default = "k8stest"
 }
 
-variable cluster_name {
+variable "cluster_name" {
   default = "k8stest"
 }
 
-variable resource_group {
+variable "resource_group" {
   default = "kubernetes"
 }
