881aa6e03f81ca67484b641cabdefcf75d23ba72
Adam <01169179@pw.edu.pl>
2023-08-02 13:29:26 +0200

    Changed eks managed node groups

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,14 +1,11 @@
-# aws --version
-# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
-# AKIA4AHVNOD7OOO6T4KI
+
 
 # konfiguracja backendu w s3
 terraform {
   backend "s3" {
-    bucket = "mybucket"       # Will be overridden from build
-    key    = "path/to/my/key" # Will be overridden from build
+    bucket = "mybucket"       # Te wartości zostaną i tak nadpisane przez Azure Devops
+    key    = "path/to/my/key" # Te wartości zostaną i tak nadpisane przez Azure Devops
     region = "us-east-1"
   }
 }
@@ -22,17 +19,19 @@ data "aws_subnet_ids" "subnets" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 2.12"
+  //>>Uncomment this section once EKS is created - Start
+  # host                   = data.aws_eks_cluster.cluster.endpoint
+  # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  # token                  = data.aws_eks_cluster_auth.cluster.token
+  # version                = "~> 2.12"
+  //>>Uncomment this section once EKS is created - End
 }
 
 module "in28minutes-cluster" {                      # korzystamy z modułu by stworzyć klaster k8s
   source          = "terraform-aws-modules/eks/aws" # moduł udostępniony przez samego Terraforma do stworzenia EKS
   cluster_name    = "project2-eks-cluster"
   cluster_version = "1.27"
-  subnets         = ["subnet-0945d29a182b2c243", "subnet-0c4c0437af075b65e"] # wersja statyczna z naszymi podsieciami będącymi w domyślnym VPC
+  subnets         = ["subnet-0945d29a182b2c243", "subnet-0c4c0437af075b65e"] # wersja statyczna z naszymi podsieciami będącymi w domyślnym VPC, # Donot choose subnet from us-east-1e
   # subnets = data.aws_subnet_ids.subnets.ids  # wersja z dynamicznym wyborem podsieci
   vpc_id = aws_default_vpc.default.id # ustawiamy w jakim VPC ma powstać klaster [w środowisku produkcyjnym normalnie byśmy skorzystali z customowego VPC]
 
@@ -40,28 +39,38 @@ module "in28minutes-cluster" {                      # korzystamy z modułu by st
 
   cluster_endpoint_public_access = true # to allow connection to the api server
 
-  # informacje o nodach w klastrze
-  node_groups = [
-    {
-      instance_type    = "t2.micro"
-      max_capacity     = 5
-      desired_capacity = 3
-      min_capacity     = 3
+  # informacje o nodach w klastrze | EKS Managed Node Group(s)
+  # Jest to sposób na dostarczanie i zarządzanie worker nodes w klastrze EKS.
+
+  eks_managed_node_group_defaults = { # domyślna konifugracja nodami (użyta zostanie jeśli nie będzie nadpisana przez eks_managed_node_groups)
+    instance_types = ["t2.small", "t2.medium"]
+  }
+
+  eks_managed_node_groups = {
+    # podział na blue i green oznacza prawdopodobnie dwa środowiska
+    blue = {} # nie ma zdefiniowanego, a zatem będzie użyta domyślna konfiguracja
+    green = {
+      min_size     = 1  # minimalna liczba nodów jaka musi być ciągle
+      max_size     = 10 # maksymalna liczba nodów jaka może być dodana do node groupa
+      desired_size = 1
+
+      instance_types = ["t2.small"]
     }
-  ]
+  }
 }
-
-# data providery
-data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
-}
-
 
 //>>Uncomment this section once EKS is created - Start
+
+# data providery
+# data "aws_eks_cluster" "cluster" {
+#   name = module.in28minutes-cluster.cluster_id
+# }
+
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.in28minutes-cluster.cluster_id
+# }
+
+
 
 # # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
 # # ServiceAccount needs permissions to create deployments and services in default namespace
